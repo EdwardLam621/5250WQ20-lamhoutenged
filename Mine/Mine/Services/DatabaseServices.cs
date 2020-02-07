4ed27bee3e80967c1f792d3b86bb38e781184c31
Edward <lamhoutenged@seattleu.edu>
2020-02-07 12:17:01 -0800

    Added ReadAsync to DatabaseService

--- Mine/Mine/Services/DatabaseServices.cs
+++ Mine/Mine/Services/DatabaseServices.cs
@@ -39,6 +39,10 @@ namespace Mine
             return Database.InsertAsync(item);
         }
 
+        public Task<ItemModel> ReadAsync(string id)
+        {
+            return Database.Table<ItemModel>().Where(i => i.Id.Equals(id)).FirstOrDefaultAsync();
+        }
 
     }
 }
